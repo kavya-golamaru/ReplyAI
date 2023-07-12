5893a32acbd2b51460441ce2587df7eb2a73c6e3
Adrien Chen <adrien.chen@ukg.com>
2023-07-11 21:12:42 -0400

    Implemented buildPrompt function

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -12,12 +12,29 @@ Office.onReady((info) => {
     document.getElementById("subject-button").onclick = setSubjectText;
     document.getElementById("body-button").onclick = setBodyText;
     document.getElementById("fetch-button").onclick = setFetchText;
+    document.getElementById("show-prompt-button").onclick = buildPrompt;
   }
 });
+
+export function getSubjectText() {
+  return Office.context.mailbox.item.subject;
+}
 
 export async function setSubjectText() {
   const subject = Office.context.mailbox.item.subject;
   document.getElementById("subject-text").innerHTML = "<b>Subject:</b> <br/>" + subject;
+}
+
+export async function getBodyText() {
+  return new Promise((resolve, reject) => {
+    Office.context.mailbox.item.body.getAsync("text", function (result) {
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+        resolve(result.value);
+      } else {
+        reject(new Error("Failed to retrieve body text."));
+      }
+    });
+  });
 }
 
 export async function setBodyText() {
@@ -36,4 +53,31 @@ export async function setFetchText() {
   const json = await response.json();
   const jsonString = JSON.stringify(json, null, 2);
   document.getElementById("fetch-text").innerHTML = "<b>Response:</b> <br/>" + jsonString;
+}
+
+export async function buildPrompt() {
+  const stringBuilder = [];
+  stringBuilder.push("Email Subject: " + getSubjectText() + " ");
+  stringBuilder.push("Email Body: " + (await getBodyText()) + " ");
+  stringBuilder.push("Response Instructions: IMPLEMENT THIS LATER ");
+  stringBuilder.push(
+    `I need assistance in crafting a response to this email. Please help me by providing a coherent 
+    and formal reply based on the given subject line and email body. Use the information provided in 
+    Response Instructions to compose the email; do not add any new information or hallucinate any 
+    details. Ensure that the response addresses the sender's concerns, but do not address the concern 
+    if there is no answer provided in Response Instructions. `
+  );
+  stringBuilder.push(
+    `Please do not generate any new information or fabricate any details beyond what is provided 
+    in the subject line, email body, and any additional context. The response should be based 
+    solely on the given information and should not add any speculative or false information.`
+  );
+  stringBuilder.push(
+    `once you finished the email, end with EMAILISFINISHED 
+    -------------- start writing ------------------------
+    `
+  );
+
+  const result = stringBuilder.join("");
+  document.getElementById("prompt-text").innerHTML = "<b>Prompt:</b> <br/>" + result;
 }
